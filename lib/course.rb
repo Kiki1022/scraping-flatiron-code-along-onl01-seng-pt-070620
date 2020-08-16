3e4c2c6d0cb7723a427bbe5f739872a01b5b0640
Kinnari Shah <kinnari1022@yahoo.com>
2020-08-16 23:18:10 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,14 +1,12 @@
-
 class Course
  
-  attr_accessor :title, :schedule, :description
-  
-  @@all = []
+ attr_accessor :title, :schedule, :description
+ @@all = []
   
   def initialize
     @@all << self
   end
-  
+
   def self.all
     @@all
   end
@@ -17,5 +15,32 @@ class Course
     @@all.clear
   end
   
+  
 end
+
+
+
+
+
+
+
+# class Course
+ 
+#   attr_accessor :title, :schedule, :description
+  
+#   @@all = []
+  
+#   def initialize
+#     @@all << self
+#   end
+  
+#   def self.all
+#     @@all
+#   end
+  
+#   def self.reset_all
+#     @@all.clear
+#   end
+  
+# end
 
